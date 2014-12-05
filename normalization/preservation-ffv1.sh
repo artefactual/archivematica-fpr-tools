ffe88b3e6ffbca3814987c68d2a78495b5eb9107
Misty De Meo <mistydemeo@gmail.com>
2014-12-04 16:41:16 -0800

    preservation-ffv1: generate framemd5 when normalizing

--- normalization/preservation-ffv1.sh
+++ normalization/preservation-ffv1.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 inputFile="%fileFullName%"
+framemd5path="%SIPDirectory%metadata/%fileUUID%.md5"
 outputFile="%outputDirectory%%prefix%%fileName%%postfix%.mkv"
 audioCodec="pcm_s16le"
 videoCodec="ffv1 -level 3"
 
 command="ffmpeg -vsync passthrough -i \"${inputFile}\" "
+# Generating framemd5 at the same time as normalization is faster
+# than doing both separately - framemd5 requires ffmpeg to decompress
+# every frame in the video, but when performe at the same time as
+# normalization, both tasks can share the same decompressed frames.
+command="${command} -f framemd5 \"${framemd5path}\" "
 command="${command} -vcodec ${videoCodec} -g 1 "
 command="${command} -acodec ${audioCodec}"
 
