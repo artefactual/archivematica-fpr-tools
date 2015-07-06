05d91bfc52e95808143f599ba2f52430b500daab
Misty De Meo <mistydemeo@gmail.com>
2015-07-06 14:11:45 -0700

    FITS: write file to disk and read output from that

--- characterization/fits.sh
+++ characterization/fits.sh
@@ -1,1 +1,8 @@
-ng edu.harvard.hul.ois.fits.Fits -i %relativeLocation%
+set -euo pipefail
+IFS=$'\n\t'
+
+tempdir=$(mktemp -d /tmp/fits.XXXXXX)
+ng edu.harvard.hul.ois.fits.Fits -i "%relativeLocation%" -o "$tempdir/fits.xml" >/dev/null
+cat "$tempdir/fits.xml"
+
+rm -r "$tempdir"
